33d17db7e5f80c7d0083be8931604507a4a6c01b
Andrew Ferguson <andrew@fergcorp.com>
2017-06-13 23:36:46 +0100

    Testing updates

--- tests/test-fergcorp-blockquote-cite.php
+++ tests/test-fergcorp-blockquote-cite.php
@@ -14,26 +14,21 @@
 class Test_Fergcorp_Blockquote_Cite extends WP_UnitTestCase {
 
 
-    private $plugin;
- 
-     function setUp() {
-         
-         parent::setUp();
-         //$this->plugin = $GLOBALS['fergcorp-blockquote-cite'];
-     
-     } // end setup
+	function setUp() {
+
+			parent::setUp();
+		
+		} // end setup
 	 
  	function test_plugin_activated() {
  		$this->assertTrue( is_plugin_active( PLUGIN_PATH ) );
  	}
 	
-/*	function test_fergcorp_blockquote_cite(){
+	function test_fergcorp_blockquote_cite(){
 		
-		$test_content = "Lorem ipsum dolor sit amet, no mea dicit inermis alienum, eum minim moderatius ut. <blockquote cite=\"http://www.example.com/path/to/file\">Sea eu antiopam theophrastus voluptatibus, cu modo dicat altera est.</blockquote> Has ad quidam moderatius, at tempor scripserit definitionem sit. Erat movet sensibus mei cu, ea mea harum vivendo singulis, inermis detracto mea ea. At nec solet definitionem, phaedrum maiestatis et mel.";
-	
-		$this->fergcorp_blockquote_cite( $test_content );
+		$test_content = 'Lorem ipsum dolor sit amet, no mea dicit inermis alienum, eum minim moderatius ut. <blockquote cite="http://www.example.com/path/to/file">Sea eu antiopam theophrastus voluptatibus, cu modo dicat altera est.</blockquote> Has ad quidam moderatius, at tempor scripserit definitionem sit. Erat movet sensibus mei cu, ea mea harum vivendo singulis, inermis detracto mea ea. At nec solet definitionem, phaedrum maiestatis et mel.';
 		
-		$this->assertEquals($test_content, $this->fergcorp_blockquote_cite( $test_content ) );
+		$this->assertEquals($test_content, fergcorp_blockquote_cite( $test_content ) );
 		
-	}*/
+	}
 }
